12a767837cd606293fa89b1f075a071a5790e6a2
Bang Script <74099045+Blaze-ID-22@users.noreply.github.com>
2021-04-25 00:00:55 +0700

    Add files via upload

---
+++ banner.php
@@ -0,0 +1,25 @@
+<?php
+
+
+echo $lblue."[-] message ".$green."selamat datang\n";
+echo $lblue."[!] server ".$green."online\n";
+echo $lblue."[?] script < ".$green."nzfun.tk".$putih." > versi : ".$yellow."1.0\n";
+echo $putih."---------------------------------------------\n";
+echo $blue."[!] creator by ".$putih."ramadhan\n";
+echo $blue."[-] whatsapp ".$putih."089608342200\n";
+echo $blue."[-] support by ".$putih."subscriber ".$green."[".date('d-m-Y')."]-[".date("H:i")."]  \n";
+echo $putih."------------------------------------------------\n";
+echo $lblue." - support my channel ".$green." -> Bang Script\n";
+echo $lblue." - support my channel ".$green." -> New Scripter\n";
+echo $lblue." - join group whatsapp ".$green."-> bit.ly/Inviteforum\n";
+echo $lblue." - join group telegram ".$green."-> t.me/BangScript\n";
+echo $putih."------------------------------------------------\n";
+echo $red."[!] warning !!\n";
+echo $yellow."[-] ini adalah prog*hbnram ilegal\n";
+echo $yellow."[-] resiko sepenuhnya di tanggung pengguna\n";
+echo $putih."------------------------------------------------\n";
+
+
+
+
+
